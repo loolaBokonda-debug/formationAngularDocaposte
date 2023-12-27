b3553dd513cc045f4668127ad670c310d59cecda
lbokonda <loola.bokonda@docaposte.fr>
2023-12-27 15:35:16 +0100

    [ngModel] : binding unidirectionnel du ts vers le html
    Ici vu après 3 sécondes, la valeur du html passe de "v1" à "value 2".
    
    Exemple du login.

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  login? : string = "v1"
+ 
+
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(()=>{
+      this.login ="value 2"
+    }, 3000
+    )
   }
 
 }
