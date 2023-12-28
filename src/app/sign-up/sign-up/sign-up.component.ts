64362090e23f9e98207584c8b069939d456ce189
lbokonda <loola.bokonda@docaposte.fr>
2023-12-28 14:49:21 +0100

    Template Driven Forms -version alpha
    Avec des lacunes

--- src/app/sign-up/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up/sign-up.component.ts
@@ -1,18 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-
+ 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-
-  login? : string ="Bokonda"
-  password? : string = "djdjdj"
-
+ 
+ 
+ 
   constructor() { }
-
+ 
+  login : string =""
+  password : string =""
+  email : string =""
+  gender : string =""
+  age : number =0
+ 
+  validateLogin() {
+    return /^[a-zA-Z]+$/.test(this.login)
+  }
+  validatePassword() {
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{4,}$/.test(this.password)
+  }
+  validateEmail() {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(this.email)
+  }
+  validateGender() {
+    return ['m', 'f '].includes(this.gender)
+  }
+  validateAge() {
+    return this.age > 10 && this.age < 120
+  }
+ 
+  valider(){
+    if (this.validateLogin() && this.validatePassword() && this.validateEmail() && this.validateGender() && this.validateAge()){
+       console.log("success")
+    }
+  }
+ 
   ngOnInit(): void {
   }
-
+ 
 }
